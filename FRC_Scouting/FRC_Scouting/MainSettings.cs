779561668d2d9fa3a2b074adaac0c03c1ba822e2
xNovax <nova1104@live.com>
2014-07-06 20:09:38 -0400

    Added MainSettings.cs

---
+++ FRC_Scouting/FRC_Scouting/MainSettings.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace FRC_Scouting
+{
+    public partial class MainSettings : Form
+    {
+        public MainSettings()
+        {
+            InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void clearAllSettingsButton_Click(object sender, EventArgs e)
+        {
+            FRC_Scouting.Properties.Settings.Default.Reset();
+            FRC_Scouting.Properties.Settings.Default.Save();
+        }
+    }
+}
